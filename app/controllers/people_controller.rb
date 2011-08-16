cac58f780f9e0671c2d320e4a1f4cc78fbf9848c
John Dalton <john@johndalton.info>
2011-08-16 16:59:11 +1000

    Harmonizing with distributed code for part 2 of the peepcode rails 3 tute.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,20 +1,20 @@
 class PeopleController < ApplicationController
 
   before_filter :authenticate, :only => :show
-  
+
   def index
     @people = [
-      'luke-skywalker',
-      'darth-vader'
-    ]
+        'luke-skywalker',
+        'darth-vader'
+      ]
   end
-
+  
   def show
     @person = params[:id]
   end
 
   private
-
+  
   def authenticate
     if params[:id] == "darth-vader"
       redirect_to people_url, :notice => "That page is restricted."
